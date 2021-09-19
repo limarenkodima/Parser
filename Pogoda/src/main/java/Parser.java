8a011d0bf376bfb1fd206dc968b9c46eb07fc826
dima <limarenko.it22@mail.ru>
2021-09-19 21:33:40 +0300

    Добавлен частный случай, который ломал программу. Добавлены коментарии.

--- Pogoda/src/main/java/Parser.java
+++ Pogoda/src/main/java/Parser.java
@@ -11,28 +11,44 @@ import java.util.regex.Pattern;
 
 public class Parser {
     private static Document getPage() throws IOException {
-       String url = "http://pogoda.msk.ru/";
-       Document page = Jsoup.parse(new URL(url), 3000);
-       return page;
+       String url = "http://pogoda.msk.ru/"; // URL сайта, с которо происходит сбор информации
+        Document page = Jsoup.parse(new URL(url), 3000);
+        // parse формирует объект Document page и собирает страницу
+        return page;
     }
 
     private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
 
     private static String getDateFromString(String stringDate) throws Exception {
         Matcher matcher = pattern.matcher(stringDate);
-        if (matcher.find()){
-            return matcher.group();
-        }
-        throw new Exception("Не смог найти");
+            if (matcher.find()){
+                return matcher.group();
+            }
+                throw new Exception("Не смог найти");
     }
 
     private static int printPartValues(Elements values, int index){
-        int iterationCount = 4;
+        int iterationCount = 4; // в каждой графе в общем случае всего 4 элемента (Утро День Вечер Ночь)
         if (index == 0) {
             Element valueLn = values.get(index);
+                boolean isMorning = valueLn.text().contains("Ночь");
+            //int iterationCount = 4;
+                    if (isMorning) {
+                        iterationCount = 5; // Частный случай, когда (Ночь Утро День Вечер Ночь)
+                    }
+                        for (int i = 0; i < iterationCount; i++) {
+                            Element valueLine = values.get(index + i);
+                                for (Element td : valueLine.select("td")) {
+                                System.out.print(td.text() + "    ");
+                        }
+                                System.out.println();
+                        }
+                        return iterationCount;
 
+        }
+        else {
             for(int i = 0; i < iterationCount; i++){
-            Element valueLine = values.get(index + i);
+                Element valueLine = values.get(index + i);
             for(Element td : valueLine.select("td")){
                System.out.print(td.text() + "    ");
             }
@@ -56,8 +72,5 @@ public class Parser {
             int iterationCount =  printPartValues(values, index);
             index = index + iterationCount;
         }
-        //System.out.println(tableWth);
-        //String date = "";
-        //System.out.println(date+ "  Явления  Температура  Давление  Влажность  Ветер");
     }
 }
